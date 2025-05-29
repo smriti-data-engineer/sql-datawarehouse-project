9cffafd31c8497cb63c337fdcfb62d84a719c2e1
smriti-data-engineer <aarya250596@gmail.com>
2025-05-29 21:11:16 +0530

    Correct column name

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -5,7 +5,7 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_key VARCHAR(50),
 	cst_firstname VARCHAR(50),
 	cst_lastname VARCHAR(50),
-	cst_matrital_status VARCHAR(50),
+	cst_marital_status VARCHAR(50),
 	cst_gndr VARCHAR(50),
 	cst_create_date DATE
 );
