cd2c4e84a2a6472e20c4ba662b0f63a25bda604b
smriti-data-engineer <aarya250596@gmail.com>
2025-05-30 19:01:47 +0530

    Add load scrip for gold layer

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,68 @@
+-- ====================================================================================
+-- Create Dimension: gold.dim_customers
+-- ====================================================================================
+
+CREATE OR REPLACE VIEW gold.dim_customers AS
+SELECT
+	ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	la.cntry AS country,
+	ci.cst_marital_status AS marital_status,
+	CASE 
+		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+		ELSE COALESCE(ca.gen, 'n/a')
+	END gender,
+	ca.bdate AS birthdate,
+	ci.cst_create_date AS create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key = la.cid
+ORDER BY customer_id;
+
+
+-- ====================================================================================
+-- Create Dimension: gold.dim_products
+-- ====================================================================================
+CREATE OR REPLACE VIEW gold.dim_products AS
+SELECT
+	ROW_NUMBER() OVER(ORDER BY pi.prd_start_dt, pi.prd_key) AS product_key,
+	pi.prd_id AS product_id,
+	pi.prd_key AS product_number,
+	pi.prd_nm AS product_name,
+	pi.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat subcategory,
+	pc.maintenance,
+	pi.prd_cost AS cost,
+	pi.prd_line AS product_line,
+	pi.prd_start_dt AS start_date
+FROM silver.crm_prd_info pi
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pi.cat_id = pc.id
+WHERE prd_end_dt IS NULL; -- Filter out historical data
+
+
+-- ====================================================================================
+-- Create Fact: gold.fact_sales
+-- ====================================================================================
+CREATE OR REPLACE VIEW gold.fact_sales AS
+SELECT
+	sls_ord_num AS order_number,
+	pr.product_key,
+	cu.customer_key,
+	sls_order_dt AS order_date,
+	sls_ship_dt AS shipping_date,
+	sls_due_dt AS due_date,
+	sls_sales AS sales_amount,
+	sls_quantity AS quantity,
+	sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id
